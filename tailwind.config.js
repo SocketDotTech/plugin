e3bcf61a966970c5f6e69113e3c4daccb8ba5045
salil-naik <salil.naik27@gmail.com>
2022-06-24 19:30:39 +0530

    fix: tailwind bundled into the package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  enabled: process.env.NODE_ENV === 'publish',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
--- test/src/App.js
+++ test/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import { Bridge } from "widget";
 
 function App() {
