2fe2e67f2f26711c782b5d1e50fd46c90d7dd3f1
salil-naik <salil.naik27@gmail.com>
2023-08-21 22:08:37 +0530

    feat: subscribed to ethereum onchange events in react example

--- examples/with-react/src/App.tsx
+++ examples/with-react/src/App.tsx
@@ -1,10 +1,20 @@
 import { Bridge } from "@socket.tech/plugin";
 import { Header } from "./components/Header";
+import { useState } from "react";
 
 export const App = () => {
+  const [provider, setProvider] = useState<any>();
+
+  /**
+   * uplifting the provider from header as i'm not using context of global state management
+   */
+
+  const upliftProvider = (value: any) => {
+    setProvider(value);
+  };
   return (
     <div className="bg-slate-900 h-screen">
-      <Header />
+      <Header upliftProvider={(value: any) => upliftProvider(value)} />
       <div className="flex flex-col items-center mt-10">
         <h1 className="text-5xl text-white font-semibold mb-6">
           Socket Plugin with React
@@ -14,6 +24,7 @@ export const App = () => {
         </p>
         <Bridge
           API_KEY="72a5b4b0-e727-48be-8aa1-5da9d62fe635" // test api key
+          provider={provider}
         />
       </div>
     </div>
