9458fe8bcff8d7c45cc64fb1719d5ca363ccfe99
Salil Naik <salil.naik27@gmail.com>
2023-08-24 16:35:17 +0530

    fix: default token setting (#82)
    * fix: default token setting on first render
    
    * updated examples
    
    * v1.2.4
    
    * chores: updated examples with latest version

--- examples/with-next/pages/index.tsx
+++ examples/with-next/pages/index.tsx
@@ -3,12 +3,13 @@ import { Header } from "../components/Header";
 import dynamic from "next/dynamic";
 import { useNetwork } from "wagmi";
 import { useEthersSigner } from "../adapters/useEthersSigner";
+import { useState } from "react";
 
 /**
  * Socket Plugin references localStorage.
  * Since Next js renders the components on the server, the code that references localStorage
  * is being executed on the server, where localStorage is not available
- * 
+ *
  * Hence Plugin is imported dynamically
  */
 const DynamicBridge = dynamic(
@@ -19,11 +20,12 @@ const DynamicBridge = dynamic(
   }
 );
 
-
 const Home: NextPage = () => {
-  const {chain} = useNetwork()
-  const signer = useEthersSigner({chainId: chain?.id});
+  const { chain } = useNetwork();
+  const signer = useEthersSigner({ chainId: chain?.id });
   const provider = signer?.provider;
+
+  const [showBridge, setShowBridge] = useState<boolean>(true);
 
   return (
     <div className="bg-slate-900 h-screen flex flex-col justify-between">
@@ -38,10 +40,23 @@ const Home: NextPage = () => {
           <p className="text-slate-400 mb-10">
             This example uses Nextjs, Tailwind CSS, Wagmi, Viem and Rainbowkit
           </p>
-          <DynamicBridge
-            API_KEY="72a5b4b0-e727-48be-8aa1-5da9d62fe635" // test api key
-            provider={provider}
-          />
+          <button
+            className="mb-10 bg-white rounded p-3"
+            onClick={() => setShowBridge(!showBridge)}
+          >
+            Toggle Bridge
+          </button>
+          {showBridge && (
+            <DynamicBridge
+              API_KEY="72a5b4b0-e727-48be-8aa1-5da9d62fe635" // test api key
+              provider={provider}
+              defaultSourceNetwork={137}
+              defaultDestNetwork={10}
+              enableSameChainSwaps
+              defaultSourceToken="0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+              defaultDestToken="0x374ad0f47f4ca39c78e5cc54f1c9e426ff8f231a"
+            />
+          )}
         </div>
       </div>
       <footer className="text-gray-200 text-center p-6">
